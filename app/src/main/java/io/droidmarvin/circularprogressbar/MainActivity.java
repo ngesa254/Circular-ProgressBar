d4960a49a2229a57afc23ba8d6b7e5630c6fad2f
ngesa marvin <marvinngesa@gmail.com>
2018-02-07 06:35:00 +0300

    update main activity

--- app/src/main/java/io/droidmarvin/circularprogressbar/MainActivity.java
+++ app/src/main/java/io/droidmarvin/circularprogressbar/MainActivity.java
@@ -2,12 +2,19 @@ package io.droidmarvin.circularprogressbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.rey.material.widget.ProgressView;
 
-public class MainActivity extends AppCompatActivity {
+public class MaterialDesignAndroidCircularProgress extends AppCompatActivity {
+
+    ProgressView circularProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.material_design_circular_progressbar);
+
+        circularProgressBar = (ProgressView) findViewById(R.id.circular_progress);
+        circularProgressBar = (ProgressView) findViewById(R.id.circular_progress1);
+        circularProgressBar = (ProgressView) findViewById(R.id.circular_progress2);
     }
 }
